a3d9b865ed9c65f207c1b027074dd8fa810fc30b
Bundy <bundyletsplay@gmail.com>
2025-08-03 23:57:49 +0200

    changed some things

---
+++ src/main/java/net/bundy/jsmp/item/custom/FractionDevice.java
@@ -0,0 +1,11 @@
+package net.bundy.jsmp.item.custom;
+
+import net.minecraft.world.item.Item;
+
+public class FractionDevice extends Item{
+    public FractionDevice(Properties properties) {
+        super(properties);
+    }
+
+
+}
